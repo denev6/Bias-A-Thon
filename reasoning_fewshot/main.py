5395515cf58f536cae714abbcd70324325db3c1a
Park Jeong-su <bakjs0422@naver.com>
2025-05-14 12:18:08 +0900

    update: reasoning_fewshot directory

--- reasoning_fewshot/main.py
+++ reasoning_fewshot/main.py
@@ -7,8 +7,7 @@ from config import *
 from model import Model, set_cuda, collect_garbage
 from prompt import (
     preprocess,
-    generate_prompt_for_masking,
-    generate_masked_prompt,
+    generate_full_prompt,
     split_answer,
     extract_last_choice,
 )
@@ -42,77 +41,13 @@ our_llm = Model(
     device_map=MODEL_DEVICE_MAP,
 )
 
-
-###########################################################
-
-
-file_prefix = "masking"
-
-# 원본 데이터 및 체크포인트
-print("🔥데이터 준비 중...")
-df_original, df_check_point, start_idx = load_data(
-    path=join_path(INPUT_CSV),
-    cols=["ID", "masked"],
-    checkpoint_dir=join_path(CHECKPOINT_DIR),
-    last_checkpoint=LAST_MASKING_CHECK_POINT,
-    prefix=file_prefix,
-)
-total_data_size = len(df_check_point)
-
-# 마스킹할 프롬프트 준비
-prompts = preprocess(
-    df_original,
-    function=generate_prompt_for_masking,
-    num_workers=NUM_WORKERS,
-)
-
-# 원본 데이터 마스킹
-print("🔥마스킹을 시작합니다!")
-collect_garbage()
-
-start_time = time.time()
-while start_idx < total_data_size:
-    end_idx = min(start_idx + BATCH_SIZE, total_data_size)
-    batch_prompts = prompts[start_idx:end_idx]
-    batch_answers = pipeline(our_llm, batch_prompts, max_new_tokens=MAX_NEW_TOKENS)
-
-    for idx, answer in enumerate(batch_answers):
-        idx = idx + start_idx
-        _, raw_answer = split_answer(answer)
-        df_check_point.at[idx, "masked"] = raw_answer.strip()
-
-        if idx % CHECK_POINT_STEP == 0:
-            # Check point에서 답변을 파일로 저장
-            end_time = time.time()
-            save_data(
-                df_check_point,
-                cols=["ID", "masked"],
-                path=join_path(CHECKPOINT_DIR, f"{file_prefix}_{str(idx)}.csv"),
-            )
-            print(f"✅[{idx}/{total_data_size}] {(end_time - start_time) / 60:.1f}분")
-            start_time = time.time()
-
-    start_idx = end_idx
-
-# 새로 마스킹된 context로 저장
-df_original["context"] = df_check_point["masked"]
-
-# 최종 파일 저장
-save_data(
-    df_original,
-    cols=["ID", "context", "question", "choices"],
-    path=join_path(MASKED_CSV),
-)
-print("\n😝마스킹이 완료되었습니다.")
-
-
-###########################################################
+#######################################
 
 
 file_prefix = "submit"
 
 df_original, df_check_point, start_idx = load_data(
-    path=join_path(MASKED_CSV),
+    path=join_path(INPUT_CSV),
     cols=["ID", "raw_input", "raw_output", "answer"],
     checkpoint_dir=join_path(CHECKPOINT_DIR),
     last_checkpoint=LAST_INFERENCE_CHECK_POINT,
@@ -123,7 +58,7 @@ total_data_size = len(df_check_point)
 # 마스킹된 프롬프트 준비
 prompts = preprocess(
     df_original,
-    function=generate_masked_prompt,
+    function=generate_full_prompt,
     num_workers=NUM_WORKERS,
 )
 
