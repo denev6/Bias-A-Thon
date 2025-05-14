8f13c0bbf9920e15a0cfada37b74407431ef3356
denev6 <sungjin.code@gmail.com>
2025-05-14 19:25:26 +0900

    refactor: remove unnecessaries and replace prompts with the current best

--- reasoning_fewshot/config.py
+++ reasoning_fewshot/config.py
@@ -1,13 +1,11 @@
 ## 주요 설정
 BASE_DIR = "./"  # 작업 경로를 설정해 주세요
 INPUT_CSV = "test.csv"  # 입력 csv
-MASKED_CSV = "masked.csv"  # 중간 마스킹을 저장할 csv
 FINAL_CSV = "submission.csv"  # 출력 csv
 MODEL_DIR = "llama3"  # 모델 디렉토리
 CHECKPOINT_DIR = "checkpoint"
 MODEL_DEVICE_MAP = "auto"  # GPU 상황에 맞게 변경하세요
 # (int) 이전에 저장한 체크포인트 (없으면 자동으로 처음부터 탐색)
-LAST_MASKING_CHECK_POINT = 0
 LAST_INFERENCE_CHECK_POINT = 0
 BATCH_SIZE = 64  # CUDA-Out-of-Memory가 발생하면 2배수로 줄여가며 실행해 보세요
 
@@ -19,7 +17,7 @@ SKIP_SPECIAL_TOKENS = True
 DO_SAMPLE = False
 TEMPERATURE = 0.6
 TOP_K = 0.9
-MAX_NEW_TOKENS = 256
+MAX_NEW_TOKENS = 8
 TOKENIZER_MAX_LENGTH = 2048
 CHECK_POINT_STEP = 500
 RANDOM_SEED = 42
